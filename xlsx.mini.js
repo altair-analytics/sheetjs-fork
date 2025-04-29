2b16d5b0cb359abf1ba14c244de0f8c0c6dd95b0
bivashy <botyrbojey@gmail.com>
2025-04-29 17:18:16 +0500

    Update build

--- xlsx.mini.js
+++ xlsx.mini.js
@@ -7259,7 +7259,6 @@ function process_col(coll) {
 }
 
 var DEF_PPI = 96, PPI = DEF_PPI;
-function px2pt(px) { return px * 96 / PPI; }
 function pt2px(pt) { return pt * PPI / 96; }
 
 /* [MS-EXSPXML3] 2.4.54 ST_enmPattern */
